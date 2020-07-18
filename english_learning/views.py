3e44eaee06e603731f34ad7b5c017080d410fb7b
kuznetsov_danil <danya.kuznetsov76@gmail.com>
2020-07-18 18:50:36 +0500

    correct views

--- english_learning/views.py
+++ english_learning/views.py
@@ -6,30 +6,30 @@ from .permissions import Check_API_KEY_Auth
 
 
 class CategoryList(generics.ListAPIView):
-    # permission_classes = (Check_API_KEY_Auth,)
+    permission_classes = (Check_API_KEY_Auth,)
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
 class LevelList(generics.ListAPIView):
-    # permission_classes = (Check_API_KEY_Auth,)
+    permission_classes = (Check_API_KEY_Auth,)
     queryset = Level.objects.all()
     serializer_class = LevelSerializer
 
 
 class ThemeList(generics.ListAPIView):
-    # permission_classes = (Check_API_KEY_Auth,)
+    permission_classes = (Check_API_KEY_Auth,)
     queryset = Theme.objects.all()
     serializer_class = ThemeListSerializer
 
 
 class ThemeDetail(generics.RetrieveAPIView):
-    # permission_classes = (Check_API_KEY_Auth,)
+    permission_classes = (Check_API_KEY_Auth,)
     queryset = Theme.objects.all()
     serializer_class = ThemeDetailSerializer
 
 
 class WordDetail(generics.RetrieveAPIView):
-    # permission_classes = (Check_API_KEY_Auth,)
+    permission_classes = (Check_API_KEY_Auth,)
     queryset = Word.objects.all()
     serializer_class = WordSerializer
